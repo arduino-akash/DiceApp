5141a140da1389706e736b8751ed9f5a3a1099e3
akash.a.singh <107540898+akash-arduino@users.noreply.github.com>
2022-09-30 12:55:16 +0530

    Initial Commit

---
+++ DiceApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiceApp
+//
+//  Created by Singh, Akash | RIEPL on 30/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
